4fca66576192a9100857e192b076ada18d722c4b
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-07-15 00:27:15 +0100

    Software is eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+* main - A program that prints all the possible combinations
+* of 2 two-digit spaced and comma separated numbers without
+* duplicates
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int ones;
+	int tens;
+	int tenz;
+	int onez;
+
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for (ones = '0'; ones <= '9'; ones++)
+		{
+			for (tenz = '0'; tenz <= '9'; tenz++)
+			{
+				for (onez = (tenz + 1); onez <= '9'; onez++)
+				{
+					putchar(tens);
+					putchar(ones);
+					putchar(' ');
+					putchar(tenz);
+					putchar(onez);
+					if (!((tens == '8' && ones == '9')
+					&& (tenz == '9' && onez == '9')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				onez = '0';
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
