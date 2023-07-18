ff82a3dda705f0e478be1d3a1e2f1e91f4fd2359
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-07-18 15:34:01 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - A function that adds two numbers and returns the result
+ * @a: First numbers of type int
+ * @b: Second number of type int
+ *
+ * Return: The result of a and b
+ */
+
+int add(int a, int b)
+{
+	int total;
+
+	total = a + b;
+
+	return (total);
+}
