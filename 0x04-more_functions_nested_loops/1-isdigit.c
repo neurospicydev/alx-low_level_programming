01a5d3ad30f5851417b7418955b8d85332317e20
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-07-20 15:26:06 +0100

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _isdigit - A function that checks for a number
+* 0 through 9
+* @n: integer to check
+*
+* Return: 1 if n is a number, and 0 otherwise
+*/
+
+int _isdigit(int n)
+{
+	if ((n >= 'a' && n <= 'z') || (n >= 'A' && n <= 'Z'))
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+	printf("\n");
+
+}
