ac6c1d5a87cad82fd421c3a400a9f975d33dc9dd
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-07-24 20:41:08 +0100

    I have not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* print_rev - A function that prints a string
+* followed by a new line
+* @s: pointer parameter of type char
+*
+* Return: Void functions have no return value
+*/
+
+void print_rev(char *s)
+{
+	int len = 0;
+	int count;
+	char *start = s;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	count = len - 1;
+	s = start;
+	for (; count > 0; count--)
+	{
+		if (*s != '\0')
+		{
+			_putchar(s[count]);
+		}
+	}
+	_putchar('\n');
+}
