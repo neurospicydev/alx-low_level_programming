fe7f3eb1329fca4b08d19fa5efe2849a0652b2b4
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-07-25 00:19:11 +0100

    winning is only half of it

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+* puts_half - A function that prints half of a string
+* followed by a new line.
+* @str: parameter/variable pointer of type char
+*
+* Return: Void functions have no return value
+*/
+
+void puts_half(char *str)
+{
+	int n, i;
+	int len = 0;
+	char *start = str;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+
+	str = start;
+	if (len % 2 != 0)
+	{
+		n = ((len - 1) / 2);
+	}
+	else
+	{
+		n = (len / 2);
+	}
+
+	for (i = n; i < len; i++)
+	{
+		if (*(str + n) == '\0')
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
