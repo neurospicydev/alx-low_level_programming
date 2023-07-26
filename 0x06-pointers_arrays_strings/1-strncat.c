4037c2881f3c7b34aaa27e7b2c985256fdc694df
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-07-26 23:19:35 +0100

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -21,11 +21,11 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		ptr++;
 	}
-	
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[len + i] = src[i];
 	}
 	dest[len + i] = '\0';
-	return(dest);
+	return (dest);
 }
