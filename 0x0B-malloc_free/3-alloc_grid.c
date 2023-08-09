ee6c05d5bb4b57c934d022955e501c3ceb2c55da
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-08-08 21:53:40 -0700

    better wake up and apologise

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,14 +20,14 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	arr = malloc(sizeof(int) * height);
+	arr = (int **)malloc(sizeof(int *) * height);
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
 	for (; i < height; i++)
 	{
-		arr[i] = malloc(sizeof(int) * width);
+		arr[i] = (int *)malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
 		{
 			return (NULL);
