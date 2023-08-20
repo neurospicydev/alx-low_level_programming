8ea9e134303ef94e57fd2e3b7cd8c0818875d41f
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-08-20 04:00:50 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,10 +14,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *ptr = malloc(nmemb * size);
 
-	if (nmemb == 0 || size == 0 || ptr == NULL)
+	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
+	if (ptr == NULL)
+		return (NULL);
+
 	for (i = 0; i < nmemb * size; i++)
 		*(ptr + i) = 0;
 	return (ptr);
