fb7cbe10c012b0b98b574f267e9065ad14485cb1
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-08-21 05:30:07 -0700

    your dog loves you

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include "dog.h"
 #include <stdlib.h>
 
@@ -15,7 +14,8 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		return
+		return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
