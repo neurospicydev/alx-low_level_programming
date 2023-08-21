4f30fb38ffb73d16ed3eccacdad531299a2a6c89
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-08-21 03:56:13 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Define a struct called dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: A structure defining a dog with members; name, age and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
