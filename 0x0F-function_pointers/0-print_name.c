453bb9f556aea76f059d91c4bc3c1171e22ab1a0
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-08-22 08:49:22 -0700

    what is my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - A function that prints a name
+ * @name: Name to be printed
+ * @f: function pointer parameter that takes one argument of type char
+ *
+ * Return: Void functions have no return value
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
