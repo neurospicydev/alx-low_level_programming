760894c8f336107cae68d8df6bf3b551731c7d1e
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-08-24 14:57:39 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,15 +11,14 @@
 
 size_t print_list(const list_t *h)
 {
-
 	size_t len = 0;
 
-	if (h->str == NULL)
-		printf("[0] (nil)\n");
 	while (h != NULL)
 	{
 		if (h->str != NULL)
 			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
 		h = h->next;
 		len++;
 	}
