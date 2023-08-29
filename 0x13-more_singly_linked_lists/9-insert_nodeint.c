d2acfd70efd880c4eb885c89faa624c68121753d
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-08-29 15:37:20 -0700

    insert node at specific index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node;
 	listint_t *curr;
-	
+
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
