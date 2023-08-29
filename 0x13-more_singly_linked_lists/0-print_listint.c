3700b2071caf2225741f9384d4712c016324cb82
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-08-29 07:04:27 -0700

    print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,10 +14,7 @@ size_t print_listint(const listint_t *h)
 	size_t nodes;
 
 	nodes = 0;
-
-	if (h == NULL)
-		return (-1);
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
