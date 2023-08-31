5e16ae3a8e7bd0962e6914265e26039234c1212f
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-08-31 15:39:20 -0700

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,43 +11,12 @@
 
 void print_binary(unsigned long int n)
 {
-	char b[MAX_BINARY_LEN];
-	unsigned long int rmdr = 0;
-	int i = 0;
-	int left_side, right_side;
-
-	if (n == 0)
+	if (n >> 0)
 	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
 		_putchar('0');
-		return;
-	}
-	while (n > 0)
-	{
-		rmdr = n % 2; /*keep track of remainder*/
-		b[i] = rmdr + '0';
-		n /= 2; /* divide n by 2 until 0 */
-		i++;
-	}
-	b[i] = '\0'; /* null terminator */
-
-	/* reverse string for correct binary representation */
-	left_side = 0;
-	right_side = i - 1;
-
-	while (left_side < right_side)
-	{
-		char tmp = b[left_side];
-
-		b[left_side] = b[right_side];
-		b[right_side] = tmp;
-		left_side++;
-		right_side--;
-	}
-	/* print binary representation */
-	i = 0;
-	while (b[i] != '\0')
-	{
-		_putchar(b[i]);
-		i++;
-	}
 }
