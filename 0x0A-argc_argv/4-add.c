12f028fc3c19f04ad1f5fbd309a80dd3a868297f
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-10-27 13:53:51 -0700

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main - A function that adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	if (argc <= 2)
+	{
+		printf("%d\n", 0);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j]; j++)
+			{
+				if (!isdigit(argv[i][j]) && (argv[i][j] != '-' || j != 0))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
