a502fdc0770e5008c613bd0bd484d84407599fc9
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-11-02 14:33:47 -0700

    arr_range, betty style doc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,11 +16,11 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	size = (max - min) + 1; 
+	size = (max - min) + 1;
 	p = (int *)malloc(size * sizeof(int));
 	if (p == NULL)
 		return (NULL);
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		p[i] = min;
 		min = min + 1;
