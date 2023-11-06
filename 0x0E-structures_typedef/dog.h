d74c44ba3766fa8b9ddc9248d6154651f97f744b
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-11-06 13:51:12 -0800

    Update dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdlib.h>
+
 /**
  * struct dog - A structure of a dog
  * @name: name of the dog
@@ -16,4 +18,5 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* DOG_H */
