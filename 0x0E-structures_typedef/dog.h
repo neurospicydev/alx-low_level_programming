5bc634148fe9c086ab7b0c282d64f0e304deb2d5
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-11-06 14:31:58 -0800

    Outside of a dog, a book is a man's best friend

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,6 +19,13 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - Typedef for the structure of a dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 #endif /* DOG_H */
