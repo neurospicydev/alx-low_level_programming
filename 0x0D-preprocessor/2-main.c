fc3eff9b416bf6f27fd3cbf7a23f222d5a6a8f7d
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-11-06 10:15:30 -0800

    file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - A function that prints the name of the file
+ * it was compiled from, followed by a newline
+ *
+ * Return: Alway 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", C_FILE);
+	return (0);
+}
