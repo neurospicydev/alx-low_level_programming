8b5cc66f070b2cc15781a917a386ef4214cd2ae4
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-11-16 13:01:26 -0800

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list- A function that frees a list_t list
+ * @head: Pointer to the head of the linked list
+ *
+ * Return: Void functions have no return value
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = current->next;
+		free(current->str);
+		free(current);
+	}
+}
