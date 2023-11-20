40be657a5e84352aed541a89b2fe09569ec2f621
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-11-20 15:50:08 -0800

    sum list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * sum_listint - A function that add the data (n)
+ * of all the nodes in a linked list
+ *
+ * @head: Pointer to the head of the linked list
+ *
+ * Return: Sum of all data (n) of a linked list,
+ * or 0 if the list is empty
+ */
+
+int sum_listint(listint_t *head)
+{
+	int total = 0;
+	listint_t *curr;
+
+	if (head == NULL)
+		return (0);
+
+	curr = head;
+
+	while (curr != NULL)
+	{
+		total += curr->n;
+		curr = curr->next;
+	}
+	return (total);
+}
