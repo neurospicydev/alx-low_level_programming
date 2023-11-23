136a2f0fdf300c81db30fd076c04c2666ff24781
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-11-23 15:54:18 -0800

    update main.h, 0 and 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_binary - A function that prints the binary
+ * representation of a number
+ *
+ * @n: number
+ *
+ * Return: Void functions have no return value
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+		_putchar('0');
+}
+
